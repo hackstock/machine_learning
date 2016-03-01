ef3c0f95a47dbcbc050f8e0582a6b0af7583ae94
Edward Pie <hackstockpie@gmail.com>
2016-03-01 15:21:20 +0000

    Added function for calculating similarities using Euclidean distance

--- recommendations.py
+++ recommendations.py
@@ -1,3 +1,4 @@
+import math
 critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
       'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
       'The Night Listener': 3.0},
@@ -15,3 +16,17 @@ critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
      'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
       'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
      'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+
+def sim_distance(prefs,person1,person2):
+  similar_items = {}
+  for item in prefs[person1]:
+    if item in prefs[person2]:
+      similar_items[item] = 1
+
+  if len(similar_items) == 0 :return 0
+
+  sum_of_squares = sum([math.pow(prefs[person1][item] - prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
+
+  return 1/(1 + sum_of_squares)
+
